41f98321ce71255623c4f7b4a1b227484b58aef5
Dashup <git@dashup.io>
2020-09-30 12:12:24 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// require first
+const { Module } = require('@dashup/module');
+
+// import base
+const SMSAction = require('./actions/sms');
+
+/**
+ * export module
+ */
+class PhoneModule extends Module {
+
+  /**
+   * construct discord module
+   */
+  constructor() {
+    // run super
+    super();
+  }
+  
+  /**
+   * Register all action interfaces here
+   * 
+   * ```
+   * // register connect class
+   * register(Action);
+   * ```
+   * 
+   * Class `Action` should extend `require('@dashup/module').Action`
+   * 
+   * @param {Function} register 
+   */
+  actions(register) {
+    // register sms action
+    register(SMSAction);
+  }
+}
+
+// create new
+module.exports = new PhoneModule();
